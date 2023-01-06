c27a5bc01d6badda227ed17cb1626a5ff4566407
AmirShafikhani <amirshafikhani.official@gmail.com>
2023-01-06 11:32:16 +0330

    get static paths added

--- pages/shop/[productId].js
+++ pages/shop/[productId].js
@@ -1,23 +1,38 @@
-import axios from 'axios';
-import React from 'react';
+import axios from "axios";
+import React from "react";
 
-const ProductDetails = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const ProductDetails = ({ productData }) => {
+  const { title, description, price } = productData;
+  return (
+    <>
+      <h2>{title}</h2>
+      <h2>{description}</h2>
+      <h2>{price}</h2>
+    </>
+  );
 };
 
 export default ProductDetails;
 
+export const getStaticPaths = async () => {
+  const { data } = await axios.get("https://fakestoreapi.com/products");
+  const paths = data.map((item) => ({ params: { productId: `${item?.id}` } }));
+
+  return {
+    paths,
+    fallback: true,
+  };
+};
+
 export const getStaticProps = async (context) => {
+  const { params } = context;
+  const { data } = await axios.get(
+    `https://fakestoreapi.com/products/${params.productId}`
+  );
 
-    const data = await axios.get("")
-
-    return {
-        props: {
-            data
-        }
-    }
-}
+  return {
+    props: {
+      productData: data,
+    },
+  };
+};
