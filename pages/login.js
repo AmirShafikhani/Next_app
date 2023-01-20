fc9a90558f161aea28940728888dcc13e1fbff9a
AmirShafikhani <amirshafikhani.official@gmail.com>
2023-01-20 13:43:58 +0330

    github connected to next auth

--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { signIn } from 'next-auth/react';
 
 const Login = () => {
     return (
         <div>
            This is login page 
+
+           <button onClick={() => signIn()}>Sign out</button>
         </div>
     );
 };
